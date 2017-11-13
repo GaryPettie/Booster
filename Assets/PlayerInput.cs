2cd15cd822cad967841ea4ef6d2f786a68bc9cd7
Gary Pettie <garypettie@gmail.com>
2017-11-13 15:57:00 +0000

    First Commit
    Added a primative rocket with basic move controls and audio.

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerInput : MonoBehaviour {
+	
+	[SerializeField] float thrustForce;
+	[SerializeField] float rotSpeed;
+
+	Rigidbody rigidbody;
+	AudioSource audioSource;
+	float thrust;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+		//Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+		//Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+		//Debug.Log("T: " + CrossPlatformInputManager.GetButton("Fire1"));
+		//Debug.Log("F: " + CrossPlatformInputManager.GetButton("Fire2"));
+		AddThrust();
+		AddRotation();
+	}
+
+	void AddThrust () {
+		float horizontal = CrossPlatformInputManager.GetAxis("Vertical");
+		if (horizontal > 0f) {
+			rigidbody.AddRelativeForce(transform.up * thrustForce * Time.deltaTime);
+			if (!audioSource.isPlaying) {
+				audioSource.Play();
+			}
+		}
+		else {
+			audioSource.Stop();
+		}
+	}
+
+	void AddRotation () {
+		float vertical = CrossPlatformInputManager.GetAxis("Horizontal");
+		if (vertical < 0f) {
+			transform.Rotate(transform.forward * rotSpeed * Time.deltaTime);
+		}
+		if (vertical > 0f) {
+			transform.Rotate(-transform.forward * rotSpeed * Time.deltaTime);
+		}
+	}
+}
