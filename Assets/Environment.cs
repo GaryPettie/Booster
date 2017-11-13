14548afea2a485ae1fb330b954b9768cf333fee3
Gary Pettie <garypettie@gmail.com>
2017-11-13 20:41:39 +0000

    Damageable
    Ship is now capable of taking damage via interface and the environment is tunable for dealing this out.  Also added a rudimentary health meter UI.

---
+++ Assets/Environment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour {
+
+	[SerializeField] float impactThreshold;
+	[SerializeField] float damageMultiplier;
+
+
+	Collider col;
+
+	void Start () {
+		col = GetComponentInChildren<Collider>();
+	}
+
+	void OnCollisionEnter (Collision other) {
+		Component damageableComponent = other.gameObject.GetComponent(typeof(IDamageable));
+		if (damageableComponent) {
+			float otherSpeed = other.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+			if (otherSpeed > impactThreshold) {
+				float damage = otherSpeed * damageMultiplier;
+				(damageableComponent as IDamageable).TakeDamage(damage);
+				Debug.Log(other.gameObject.GetComponent<Player>().currentHealth);
+			}
+		}
+	}
+}
